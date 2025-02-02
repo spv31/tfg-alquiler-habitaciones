4f2737e50319b3490e7b7cd638c96ddff792fe1d
spv31 <spv31@gcloud.ua.es>
2025-02-02 21:35:38 +0100

    chore: configurar proyecto nuxt y añadir dependencias

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,12 +2,60 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  ssr: false,
+  modules: [
+    'nuxt-auth-sanctum',
+    '@pinia/nuxt',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/i18n'
+  ],
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ]
+    plugins: [tailwindcss()],
+  },
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    }
+  ],
+  i18n: {
+    locales: ['es', 'en'],
+    vueI18n: './i18n.config.ts',
+    detectBrowserLanguage: {
+      useCookie: true,
+      redirectOn: 'root',
+    }
+  },
+  googleFonts: {
+    families: {
+      Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+    },
+    display: 'swap',
+  },
+  runtimeConfig: {
+    public: {
+      sanctum: {
+        baseUrl: "http://localhost:8000",
+        mode: "cookie",
+        endpoints: {
+          csrf: "/sanctum/csrf-cookie",
+          login: "/login",
+          logout: "/logout",
+          user: "/api/user",
+        },
+        redirect: {
+          onAuthOnly: '/login',
+          onGuestOnly: '/dashboard',
+          onLogin: "/dashboard",
+          onLogout: "/", // Landing Page / Home Page
+        },
+      },
+    }
+  },
+  imports: {
+    autoImport: true,
   }
-})
+});
---
+++ vetur.config.js
@@ -0,0 +1,11 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  projects: [
+    {
+      root: './frontend', 
+      package: './package.json', 
+      tsconfig: './tsconfig.json', 
+    }
+  ]
+}
